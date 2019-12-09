2db64af8da5793e611551fe342069d2743c1b26a
catzin <catzin@Ehecatzins-MacBook-Air.local>
2019-12-09 09:47:00 -0600

    Agrego funcionalidad para ip's clase B y clase C

---
+++ app.py
@@ -0,0 +1,217 @@
+from PyQt5.QtCore import pyqtSlot
+import sys
+from PyQt5.QtWidgets import *
+from ip import *
+from ventanaDatosIp import *
+
+class Menu:
+
+    def __init__(self):
+
+        #self.datos = datos
+        self.window = QMainWindow()
+        self.window.setWindowTitle("CrackMan-IpCalculator")
+        self.window.setStyleSheet("background:url(fondo.jpg)")
+        self.window.setFixedSize(720,580)
+        self.colocarBotones()
+        self.ipe =""
+
+    def colocarBotones(self):
+
+        self.linea = QLineEdit(self.window)
+        self.linea.setGeometry(20,22,100,30)
+        self.linea.setStyleSheet("background:#ECF0F1")
+
+        self.prefijo = QLabel(self.window)
+        self.prefijo.setGeometry(130,22,50,30)
+        self.prefijo.setStyleSheet("background:#ECF0F1")
+
+        self.clase = QLabel(self.window)
+        self.clase.setGeometry(190,22,50,30)
+        self.clase.setStyleSheet("background:#ECF0F1")
+
+        self.mask = QLabel(self.window)
+        self.mask.setGeometry(250,22,100,30)
+        self.mask.setStyleSheet("background:#ECF0F1")
+
+        self.tipe = QLabel(self.window)
+        self.tipe.setGeometry(370,22,80,30)
+        self.tipe.setStyleSheet("background:#ECF0F1")
+
+        self.info = QPushButton(self.window)
+        self.info.setText("Información")
+        self.info.setStyleSheet("background:#FEFB63")
+        self.info.setGeometry(20,60,100,30)
+        self.info.clicked.connect(self.informacion)
+
+        self.opcion = QComboBox(self.window)
+        self.opcion.addItems(["host","subredes"])
+        self.opcion.setStyleSheet("background:#FEFB63")
+        self.opcion.setGeometry(20,115,120,30)
+        #self.opcion.clicked.connect(self.informacion)
+
+        self.numSub = QLineEdit(self.window)
+        self.numSub.setGeometry(150,115,100,30)
+        self.numSub.setStyleSheet("background:#ECF0F1")
+
+        self.calcular = QPushButton(self.window)
+        self.calcular.setText("Calcular")
+        self.calcular.setGeometry(255,115,100,30)
+        self.calcular.setStyleSheet("background:#FEFB63")
+        self.calcular.clicked.connect(self.DatosCalculo)
+
+        self.respuestas = QListWidget(self.window)
+        self.respuestas.setStyleSheet("background:#ECF0F1")
+        self.respuestas.setGeometry(20,220,300,300)
+
+        self.respuestas2 = QListWidget(self.window)
+        self.respuestas2.setStyleSheet("background:#ECF0F1")
+        self.respuestas2.setGeometry(350,220,300,300)
+
+        self.datoSubredes = QLabel(self.window)
+        self.datoSubredes.setGeometry(480,115,100,30)
+        self.datoSubredes.setStyleSheet("background:#ECF0F1")
+
+        self.datoHost = QLabel(self.window)
+        self.datoHost.setGeometry(370,115,90,30)
+        self.datoHost.setStyleSheet("background:#ECF0F1")
+
+        self.maskSubred = QLabel(self.window)
+        self.maskSubred.setGeometry(605,115,110,30)
+        self.maskSubred.setStyleSheet("background:#ECF0F1")
+
+        self.subred = QPushButton(self.window)
+        self.subred.setText("Listar subRedes")
+        self.subred.setStyleSheet("background:#FEFB63")
+        self.subred.setGeometry(110,180,100,30)
+        self.subred.clicked.connect(self.listarSubredes)
+
+        self.host = QPushButton(self.window)
+        self.host.setText("Listar Host")
+        self.host.setStyleSheet("background:#FEFB63")
+        self.host.setGeometry(380,180,100,30)
+        self.host.clicked.connect(self.listarHost)
+
+        self.refrescaH = QPushButton(self.window)
+        self.refrescaH.setText("Refrescar Host!")
+        self.refrescaH.setStyleSheet("background:#FEFB63")
+        self.refrescaH.setGeometry(550,180,100,30)
+        self.refrescaH.clicked.connect(self.refrescarHost)
+
+        self.refresca = QPushButton(self.window)
+        self.refresca.setText("Refrescar!")
+        self.refresca.setStyleSheet("background:#FEFB63")
+        self.refresca.setGeometry(500,530,100,30)
+        self.refresca.clicked.connect(self.refrescar)
+
+      
+    def informacion(self):
+
+        self.entrada = self.linea.text()
+
+        if(self.entrada == ''):
+            self.error = QMessageBox.about(self.window,"Error","Duermete otro rato we")
+            self.error.show()
+        else:
+            self.ipe = ip(self.entrada)
+            self.ipe.tipo()
+            self.prefijo.setStyleSheet("background:#76D7C4")
+            self.prefijo.setText(self.ipe.prefijo)
+            self.tipe.setStyleSheet("background:#76D7C4")
+            self.tipe.setText(self.ipe.typo)
+            self.clase.setStyleSheet("background:#76D7C4")
+            self.clase.setText(self.ipe.clase)
+            self.mask.setStyleSheet("background:#76D7C4")
+            self.mask.setText(self.ipe.mask)
+    
+    def DatosCalculo(self):
+        op = self.opcion.currentText()
+        num_sub = int(self.numSub.text())
+
+        if(op == "subredes"):
+
+            self.ipe.SubNetC(num_sub)
+
+            if(self.ipe.clase is 'C'):
+                self.datoHost.setText(str(self.ipe.numHost()))
+                self.datoSubredes.setText(str(num_sub))
+                self.maskSubred.setText(self.ipe.mascaraSubred())
+            
+            elif(self.ipe.clase is 'B'):
+                self.datoHost.setText(str(self.ipe.numHost()))
+                self.datoSubredes.setText(str(num_sub))
+                self.maskSubred.setText(self.ipe.mascaraSubred())
+            
+            else:
+                self.datoHost.setText(str(self.ipe.numHost()))
+                self.datoSubredes.setText(str(num_sub))
+                self.maskSubred.setText(self.ipe.mascaraSubred())
+
+
+           
+        elif(op == "host"):
+            self.ipe.subRedesHost(num_sub)
+
+            if(self.ipe.clase is 'B'):
+                self.datoHost.setText(str(num_sub))
+                self.datoSubredes.setText(str(self.ipe.numSub))
+                self.maskSubred.setText(self.ipe.mascaraSubred())
+            
+            elif(self.ipe.clase is 'C'):
+                self.datoHost.setText(str(num_sub))
+                self.datoSubredes.setText(str(self.ipe.numSub))
+                self.maskSubred.setText(self.ipe.mascaraSubred())
+            else:
+                self.datoHost.setText(str(num_sub))
+                self.datoSubredes.setText(str(self.ipe.numSub))
+                self.maskSubred.setText(self.ipe.mascaraSubred())
+
+    
+
+    def listarSubredes(self):
+        self.respuestas.addItems(self.ipe.subRedes)
+    
+
+    def listarHost(self):
+
+        if(self.ipe.clase == 'C'):
+        
+            self.ipe.calcularHostC(self.respuestas.currentItem().text())
+            self.respuestas2.addItems(self.ipe.host)
+        
+        elif(self.ipe.clase == 'B'):
+            self.ipe.calcularHostB(self.respuestas.currentItem().text())
+            self.respuestas2.addItems(self.ipe.host)
+  
+   
+        
+    
+    def refrescar(self):
+        self.linea.setText(" ")
+        self.prefijo.setText(" ")
+        self.clase.setText(" ")
+        self.mask.setText(" ")
+        self.tipe.setText(" ")
+        self.numSub.setText(" ")
+        self.datoSubredes.setText(" ")
+        self.datoHost.setText(" ")
+        self.maskSubred.setText(" ")
+        self.respuestas.clear()
+        self.respuestas2.clear()
+    
+    def refrescarHost(self):
+        self.ipe.host = [ ]
+        self.respuestas2.clear()
+ 
+      
+    def Mostrar(self):
+        self.window.show()
+      
+
+app = QApplication(sys.argv)
+
+m = Menu()
+m.Mostrar()
+
+app.exec_()
+
---
+++ ip.py
@@ -0,0 +1,520 @@
+import math
+
+class ip:
+
+    def __init__(self,ip):
+        self.ip = ip
+        self.ocupados = 0
+        self.octetos = 0
+        self.clase =""
+        self.typo =""
+        self.prefijo =""
+        self.mask =""
+        self.bits = [ ]
+        self.decimalBits = [ ]
+        self.subRedes = [ ]
+        self.host = [ ]
+        self.numSub =0
+        
+
+    def tipo(self):
+        aux = self.ip.split('.')
+
+        if(int(aux[0]) >= 0 and int(aux[0]) <= 127):
+            if(int(aux[0]) != 10):
+                self.clase = 'A'
+                self.prefijo = "/8"
+                self.mask="255.0.0.0"
+                self.typo = "Public"
+            else:
+
+                self.clase = 'NULL'
+                self.prefijo = "/8"
+                self.mask = "255.0.0.0"
+                self.typo ="Private"
+                
+
+        if(int(aux[0]) >= 128 and int(aux[0])<= 191):
+            if(int(aux[0]) == 172 and int(aux[1]) == 16 and int(aux[1]) <= 31):
+                self.clase = 'NULL'
+                self.prefijo ="/12"
+                self.mask="255.240.0.0"
+                self.typo = "Private"
+
+            else:
+                self.clase = 'B'
+                self.prefijo ="/16"
+                self.mask="255.255.0.0"
+                self.typo = "Public"
+
+        if(int(aux[0]) >= 192 and int(aux[0]) <= 223):
+            if(int(aux[1]) == 168):
+
+                self.clase ='NULL'
+                self.prefijo ="/16"
+                self.mask="255.255.0.0"
+                self.typo = "Private"
+            else:
+
+                self.clase ='C'
+                self.prefijo ="/24"
+                self.mask="255.255.255.0"
+                self.typo = "Public"
+
+    #numero de octetos que ocupo para cubrir el numero de subRedes que me piden
+    def bitsRedes(self,num_subRedes):
+
+        i = 0
+        k = 0
+        while(k < num_subRedes):
+    
+            k = pow(2,i)
+
+            i += 1
+        
+        self.ocupados = i -1
+
+        return i -1
+
+    
+    def bitsSubRedes(self,num_subRedes):
+
+        self.octetos = self.bitsRedes(num_subRedes)
+
+        cad =""
+
+        for i in range(0,num_subRedes+1):
+
+            numBits = pow(2,self.octetos-1)
+
+            while(numBits > 0):
+                if(numBits & i):
+                    cad += '1'
+                else:
+                    cad += '0'
+
+                numBits = numBits >>1
+
+            decimal = self.BinDecimal(cad)
+            x = (cad,decimal)
+            self.bits.append(x)
+           
+            #self.decimalBits.append(x)
+            cad = ""
+        #self.decimalBits.pop()
+
+    def variarBits(self):
+
+        if(self.clase is 'B'):
+
+            bitsHost = 16 - self.ocupados
+
+        elif(self.clase is 'A'):
+
+            bitsHost = 24 - self.ocupados
+
+
+        rango = pow(2,bitsHost) - 2
+        aux = [ ]
+
+        cad =""
+
+        for i in range(1,rango+1):
+
+            numBits = pow(2,bitsHost-1)
+
+            while(numBits > 0):
+                
+                if(numBits & i):
+                    cad += "1"
+                else:
+                    cad += "0"
+
+                numBits = numBits >>1
+
+            aux.append(cad)
+            cad= ""
+
+        return aux
+
+
+    def aDecimalSub(self,cad):
+        pesos = [128,64,32,16,8,4,2,1]
+        decimal1 = 0
+        decimal2 = 0
+        mitadIz = [ ]
+        mitader = [ ]
+
+        if(len(cad) > 8):
+            mitadIz = cad[:8]
+            mitader = cad[8:]
+
+            for i in range(len(mitadIz)):
+            
+                decimal1 += int(mitadIz[i]) * pesos[i]
+        
+            for j in range(len(mitader)):
+                decimal2 += int(mitader[j]) * pesos[j]
+
+            return decimal1 + decimal2
+        else:
+
+            for k in range(len(cad)):
+                decimal1 += int(cad[k]) * pesos[k]
+
+            return decimal1
+    
+    def aDecimalHost(self,cadenaBit):
+        pesos = [ ]
+        decimal = 0
+        bitsRestantes = 16 - self.ocupados
+        for i in range(bitsRestantes):
+            pesos.append(pow(2,i))
+
+        pesos = pesos[::-1]
+
+        for j in range(len(cadenaBit)):
+            decimal += int(cadenaBit[j]) * pesos[j]
+        
+
+        return decimal
+
+    def ocupadosBin(self):
+        bin =""
+
+        for i in range(self.ocupados):
+            bin += "1"
+        
+        return bin
+    
+    def BinDecimal(self,bin):
+
+        pesos = []
+        decimal = 0 
+        for i in range(len(bin)):
+            pesos.append(pow(2,i))
+    
+        pesos = pesos[::-1]
+
+        for j in range(len(bin)):
+            decimal += int(bin[j]) * pesos[j]
+
+        return decimal
+
+    def CalcularMaskSub(self):
+
+        host = pow(2,8-self.ocupados)-2
+
+        return 32 - self.octetos
+
+    def generaHostB(self,cadenaBits):
+
+        aux = self.ip.split('.')
+        ipaux = '.'.join(aux[:2])  
+        #128.2.0.0
+        bitsRestantes = 16 - self.ocupados
+        #aqui saco datos de red 8 bits
+        aux = cadenaBits[:8]
+        #aqui saco host
+        aux2 = cadenaBits[8:]
+
+        porcionRed = aux[:self.ocupados]
+        aSumar = aux[self.ocupados:]
+        host = aux2
+        respuesta = ipaux+'.'+str(self.BinDecimal(porcionRed)+self.BinDecimal(aSumar))+'.'+str(self.BinDecimal(host))
+
+        return respuesta
+    
+
+    def mapearSubred(self,numSubred):
+        
+        #cad , decimal
+        contador = 0
+        for i in range(len(self.bits)):
+            if(self.bits[i][1] == numSubred):
+                contador = i
+        
+
+        return contador
+
+
+
+    
+    def calcularHostB(self,subred):
+
+        if(self.ocupados >= 8):
+            aux = subred.split('.')
+            
+            rango = pow(2,16-self.ocupados)-2
+            for i in range(1,rango+1):
+                self.host.append('.'.join(aux[:3])+'.'+str(int(aux[3])+i))
+                
+        else:
+
+            variacionesHost = self.variarBits()
+            aux = subred.split('.')
+
+            subRedBuscada = int(aux[2])
+
+            indexBits = self.mapearSubred(subRedBuscada)
+            
+            """print("indice de la que busco",indexBits)
+            print("el valor que busco")
+            print(self.bits[indexBits])"""
+            
+        
+            for host in variacionesHost:
+                concatDatos = self.bits[indexBits][0] + host
+                #print(concatDatos)
+                self.host.append(self.generaHostB(concatDatos)+'  '+str("/"+str(16+self.ocupados)))
+ 
+
+
+    def generarHostA(self,cadenaBits):
+
+        
+        aux = self.ip.split('.')
+        #11.
+        ipaux = '.'.join(aux[:1])
+        #aqui saco datos de red 8 bits
+        aux = cadenaBits[:8]
+        #aqui saco host
+        aux2 = cadenaBits[8:]
+        porcionRed = aux[:self.ocupados]
+        aSumar = aux[:8 -self.ocupados]
+        host1 = aux2[:8]
+        host2 = aux2[8:]
+        print(porcionRed)
+        print(aSumar)
+
+        respuesta = ipaux+'.'+str(self.aDecimalSub(porcionRed)+self.BinDecimal(aSumar))+'.'+str(self.BinDecimal(host1))+'.'+str(self.BinDecimal(host2))
+
+        return respuesta
+    
+
+    def calcularHostA(self,subred):
+        
+        variacionesHost = self.variarBits()
+        aux = subred.split('.')
+        indexBuscada = self.decimalBits.index(int(aux[2]))
+
+        for host in variacionesHost:
+
+            concatDatos = self.bits[indexBuscada] + host
+            self.host.append(self.generaHostB(concatDatos)+'  '+str("/"+str(8+self.ocupados)))
+
+        
+
+
+
+
+    def calcularHostC(self,ip):
+        aux = ip.split('.')
+        ultimo = aux.pop()
+
+        ipaux = '.'.join(aux)
+
+        resta = 8 - self.octetos
+        host = pow(2,resta) - 2
+        #print(ultimo)
+        for i in range(int(ultimo)+1,int(ultimo)+host+1):
+
+            self.host.append(ipaux+'.'+str(i)+str("/"+str(24+self.ocupados)))
+    
+
+    def numHost(self):
+        if(self.clase is 'C'):
+            host = pow(2,8-self.ocupados)-2
+
+        elif(self.clase is 'B'):
+            host = pow(2,16 -self.ocupados)-2
+        
+        else:
+            host = pow(2,24-self.ocupados)-2
+        
+        return host
+    
+
+    def Bespecial(self):
+        auxip = self.ip.split('.')
+        sub = '.'.join(auxip[:2])
+        primerOcteto =""
+        segundoOcteto=""
+
+        for bit in self.bits:
+
+            aux = bit[0]
+            aux += "0"
+            primerOcteto = aux[:8]
+            segundoOcteto = aux[8:]
+            #agregamos las subRedes a la lista
+            subred = sub+'.'+str(self.aDecimalSub(primerOcteto))+'.'+str(self.aDecimalSub(segundoOcteto))
+            self.subRedes.append(subred)
+
+    def SubNetC(self,num_subRedes):
+
+        self.bitsSubRedes(num_subRedes)
+        ipaux = self.ip.split('.')
+
+        if(self.clase is 'C'):
+            ipaux.pop()
+            nueva = '.'.join(ipaux)
+
+            for bits in self.bits:
+                self.subRedes.append(nueva+'.'+str(self.aDecimalSub(bits[0])))
+                
+        if(self.clase is 'B'):
+
+            cadena = '.'.join(ipaux[:2])
+            #Si los bits > 8 eso cambia la manera de calcular Sub
+            if(self.ocupados > 8):
+
+                self.Bespecial()
+            else:
+
+                for bits in self.bits:
+                    self.subRedes.append(cadena+'.'+str(self.aDecimalSub(bits[0]))+'.'+'0')
+
+        if(self.clase is 'A'):
+
+            for bits in self.bits:
+                cadena = '.'.join(ipaux[:1])+'.'+str(self.aDecimalSub(bits[0]))+'.'+'0'+'.'+'0'
+                self.subRedes.append(cadena)
+            self.subRedes.pop()
+                
+    def mostrarDatos(self):
+        print(self.ip)
+        print(self.clase)
+        print(self.mask)
+    
+
+    def mascaraSubred(self):
+
+        octetos = self.ocupados
+        cadena = ""
+
+        for i in range(octetos):
+            cadena += "1"
+        
+        
+        if(self.clase is 'A'):
+
+            for i in range(24 - self.ocupados):
+                cadena += "0"
+            primerOcteto = cadena[:8]
+            aux = cadena[8:]
+            segundoOctecto = aux[:8]
+            tercerOcteto = aux[8:]
+
+            respuesta = "255"+'.'+str(self.aDecimalSub(primerOcteto))+'.'+str(self.aDecimalSub(segundoOctecto))+'.'+str(self.aDecimalSub(tercerOcteto))
+            return respuesta
+
+        elif(self.clase is 'B'):
+            for i in range(16-self.ocupados):
+                cadena += "0"
+            
+            primerOcteto = cadena[:8]
+            segundoOctecto = cadena[8:]
+            
+            respuesta = "255."+"255."+str(self.aDecimalSub(primerOcteto))+'.'+str(self.aDecimalSub(segundoOctecto))
+            return respuesta
+
+        else:
+            for i in range(8 - self.ocupados):
+                cadena += "0"
+            
+            primerOcteto = cadena[:8]
+            respuesta = "255."+"255."+"255."+str(self.aDecimalSub(primerOcteto))
+            
+            return respuesta
+    
+    def bitsSubRedesHost(self,num_subRedes,octetosRes):
+
+        #self.octetos = self.bitsRedes(num_subRedes)
+
+        cad =""
+
+        for i in range(0,num_subRedes+1):
+
+            numBits = pow(2,octetosRes-1)
+
+            while(numBits > 0):
+                if(numBits & i):
+                    cad += '1'
+                else:
+                    cad += '0'
+
+                numBits = numBits >>1
+
+            decimal = self.BinDecimal(cad)
+            x = (cad,decimal)
+            self.bits.append(x)
+           
+            #self.decimalBits.append(x)
+            cad = ""
+        self.bits.pop()
+
+    def subRedesHost(self,numHost):
+
+
+        x = self.bitsRedes(numHost)
+
+
+        if(pow(2,x) - 2  < numHost ):
+
+            x = x +1
+            self.ocupados = x
+          
+        
+        if(self.clase is 'B'):
+
+            bitsPAraRed = 16 - self.ocupados
+            num_sub = pow(2,bitsPAraRed)
+            self.numSub = num_sub
+            self.SubNetC(num_sub)
+        
+
+        if(self.clase is 'C'):
+
+            bitsPAraRed = 8 - self.ocupados
+            num_sub = pow(2,bitsPAraRed)
+            self.numSub = num_sub
+            self.SubNetC(num_sub)
+        
+
+        if(self.clase is 'A'):
+
+            bitsPAraRed = 24 - self.ocupados
+            num_sub = pow(2,bitsPAraRed)
+            self.numSub = num_sub
+            self.SubNetC(num_sub)
+
+        
+
+        
+      
+       
+
+
+       
+        
+
+
+
+
+
+
+
+"""entrada = "128.2.0.0"
+
+ipe = ip(entrada)
+ipe.tipo()
+
+ipe.subRedesHost(50)
+
+
+print(ipe.subRedes)"""
+
+
+
+
